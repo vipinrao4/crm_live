de9bcfa0d4cc1fab45503d9b66e8f410fa525381
Vipin Kumar Rao <146922004+vipinrao4@users.noreply.github.com>
2026-07-10 11:43:26 +0530

    fix: set custom login.html template for auth views

--- divjot_crm_backend/urls.py
+++ divjot_crm_backend/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+]from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views # Ye add karo
+from django.contrib.auth import views as auth_views
 from crm_core import views
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'), # Ye add karo
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('update-status/<int:order_id>/', views.admin_update_status, name='admin_update_status'),
     path('admin/', admin.site.urls),
     path('', include('crm_core.urls')),
